de6cdc765861d49025838a35a0d0e9af12118614
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-05-04 16:38:41 -0300

    feat(user): add service method to retrieve all users

--- src/main/java/com/fabricio/personal_finance_api/service/UserService.java
+++ src/main/java/com/fabricio/personal_finance_api/service/UserService.java
@@ -1,5 +1,6 @@
 package com.fabricio.personal_finance_api.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.fabricio.personal_finance_api.entity.User;
@@ -20,5 +21,9 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
