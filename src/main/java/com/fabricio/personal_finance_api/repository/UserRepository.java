439b6e0e577d886aac4eff263ea1d2522dc42c08
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-05-04 16:26:32 -0300

    feat(user): add repository for user persistence operations

---
+++ src/main/java/com/fabricio/personal_finance_api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.fabricio.personal_finance_api.repository;
+
+import com.fabricio.personal_finance_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
