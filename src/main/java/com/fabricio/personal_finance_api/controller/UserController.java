91133ed7810ccfbb07ed20e060937bb1132e512c
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-05-04 16:38:06 -0300

    feat(user): add endpoint to retrieve user by id

--- src/main/java/com/fabricio/personal_finance_api/controller/UserController.java
+++ src/main/java/com/fabricio/personal_finance_api/controller/UserController.java
@@ -5,6 +5,8 @@ import com.fabricio.personal_finance_api.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,10 @@ public class UserController {
     public ResponseEntity<User> create(@Valid @RequestBody User user) {
         User created = service.create(user);
         return ResponseEntity.status(201).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        return service.findById(id);
     }
 }
