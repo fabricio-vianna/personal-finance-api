9727f782d5b91408ecd1e59e3b36f974225f8636
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-05-06 19:20:24 -0300

    feat(user): introduce UserDTO with attributes, methods, and entity-based constructor

---
+++ src/main/java/com/fabricio/personal_finance_api/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.fabricio.personal_finance_api.dto;
+
+import java.io.Serializable;
+
+import com.fabricio.personal_finance_api.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDTO implements Serializable {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
