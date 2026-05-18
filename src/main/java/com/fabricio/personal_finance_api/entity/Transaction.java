3bf1dfa7aa4bee07e784cf60dca7afe6d5e39a32
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-05-18 15:38:16 -0300

    fix(transaction-entity): preserve manually defined timestamps during persistence

--- src/main/java/com/fabricio/personal_finance_api/entity/Transaction.java
+++ src/main/java/com/fabricio/personal_finance_api/entity/Transaction.java
@@ -60,7 +60,13 @@ public class Transaction implements Serializable {
 
     @PrePersist
     public void prePersist() {
-        this.createdAt = LocalDateTime.now();
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+
+        if (this.updatedAt == null) {
+            this.updatedAt = LocalDateTime.now();
+        }
     }
 
     @PreUpdate
