3546ae58ad093b06f1bfe1413d44543134afdc2e
fabricio-vianna <fab.vianna.1976@gmail.com>
2026-08-14 14:22:30 -0300

    docs: add API title, version, and description to Swagger configuration

--- src/main/java/com/fabricio/personal_finance_api/config/OpenApiConfig.java
+++ src/main/java/com/fabricio/personal_finance_api/config/OpenApiConfig.java
@@ -2,12 +2,18 @@ package com.fabricio.personal_finance_api.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
+        info = @Info(
+                title = "Personal Finance API",
+                version = "1.0",
+                description = "API REST para gerenciamento de finanças pessoais, com autenticação JWT, controle de transações, categorias e relatórios financeiros."
+        ),
         security = @SecurityRequirement(name = "bearer-key")
 )
 @SecurityScheme(
